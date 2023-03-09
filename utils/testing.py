a1a684ce709425456bf21cd8aa64171706920a3f
michele98 <calvanese.michele@gmail.com>
2023-03-09 19:18:22 +0100

    feat!: automatize saving of labelled video add estimated remaining time to testing

--- utils/testing.py
+++ utils/testing.py
@@ -9,6 +9,8 @@ import torch
 import torch.utils.data
 from torch.utils.data import DataLoader
 
+from typing import Union
+
 
 def get_maximum_coordinates(heatmaps):
     y, x = np.nonzero(heatmaps == np.max(heatmaps))
@@ -78,7 +80,9 @@ def compute_positions(net : torch.nn.Module,
 
             epoch_time = time.time() - start_time
             batch_time = epoch_time/(batch_idx+1)
-            print(prefix + f'{batch_idx+1}/{len(data_loader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step'.ljust(80), end = '\r')
+            remaining_time = batch_time * (len(data_loader)-batch_idx)
+
+            print(prefix + f'{batch_idx+1}/{len(data_loader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, remaining: {remaining_time:.0f}s'.ljust(80), end = '\r')
     print(prefix + f'{batch_idx+1}/{len(data_loader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step'.ljust(80))
 
     return np.array(true_positions), np.array(predicted_positions), np.array(min_heatmap_values), np.array(max_heatmap_values)
@@ -126,7 +130,9 @@ def compute_positions_noh(net : torch.nn.Module,
 
             epoch_time = time.time() - start_time
             batch_time = epoch_time/(batch_idx+1)
-            print(prefix + f'{batch_idx+1}/{len(data_loader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step'.ljust(80), end = '\r')
+            remaining_time = batch_time * (len(data_loader)-batch_idx)
+
+            print(prefix + f'{batch_idx+1}/{len(data_loader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, remaining: {remaining_time:.0f}s'.ljust(80), end = '\r')
     print(prefix + f'{batch_idx+1}/{len(data_loader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step'.ljust(80))
 
     return np.array(true_positions), np.array(predicted_positions)
@@ -178,14 +184,14 @@ def create_output_csv(training_configuration,
             checkpoint_filename = name
 
     # create output folder inside the checkpoint folder
-    output_folder = '_'.join(checkpoint_filename.split('.')[0].split('_')[:2])
-    output_folder += '_results'
-    output_folder = os.path.join(config._checkpoint_folder, output_folder)
+    results_folder = '_'.join(checkpoint_filename.split('.')[0].split('_')[:2])
+    results_folder += '_results'
+    results_folder = os.path.join(config._checkpoint_folder, results_folder)
 
-    if not os.path.exists(output_folder): os.makedirs(output_folder)
+    if not os.path.exists(results_folder): os.makedirs(results_folder)
 
     if backup_checkpoint:
-        shutil.copy2(os.path.join(config._checkpoint_folder, checkpoint_filename), output_folder)
+        shutil.copy2(os.path.join(config._checkpoint_folder, checkpoint_filename), results_folder)
 
     model = config.get_model()
     model.to(device)
@@ -214,7 +220,7 @@ def create_output_csv(training_configuration,
             model,
             data_loader,
             device=device,
-            heatmaps_folder=os.path.join(output_folder, f"heatmaps_{split.lower()}"))
+            heatmaps_folder=os.path.join(results_folder, f"heatmaps_{split.lower()}"))
 
         image_size = dataset.get_info()[0]['image_size']
         output_dict['min_values'] = min_values
@@ -234,7 +240,7 @@ def create_output_csv(training_configuration,
         output_dict['x_pred'] = predicted_positions[:,0]
         output_dict['y_pred'] = predicted_positions[:,1]
 
-    pd.DataFrame(output_dict).to_csv(os.path.join(output_folder, f'output_{split.lower()}.csv'), index=False)
+    pd.DataFrame(output_dict).to_csv(os.path.join(results_folder, f'output_{split.lower()}.csv'), index=False)
 
 
 def frame_generator(filename, start_frame=None, stop_frame=None, verbose=True):
@@ -328,14 +334,14 @@ def annotate_frame(frame, predicted_position, true_position=None):
     return cv2.addWeighted(annotated_frame, 0.6, frame, 0.4, 0)
 
 
-def save_labeled_video(filename_src : str,
-                       filename_dst : str,
-                       position_df : pd.DataFrame,
-                       show_ground_truth : bool = False,
-                       heatmaps_folder : str = None,
-                       start_frame : int = None,
-                       stop_frame : int = None,
-                       fps : int = None):
+def create_video(filename_src : str,
+                 filename_dst : str,
+                 position_df : pd.DataFrame,
+                 show_ground_truth : bool = False,
+                 heatmaps_folder : str = None,
+                 start_frame : int = None,
+                 stop_frame : int = None,
+                 fps : int = None):
     """Save the video with the annotated ball position.
 
     Parameters
@@ -414,3 +420,99 @@ def save_labeled_video(filename_src : str,
 
     out.release()
     print('done.')
+
+
+def save_labeled_video(training_configuration,
+                       checkpoint_filename: str = None,
+                       split: str ='test',
+                       dataset_id: Union[int, list] = None,
+                       show_ground_truth: bool = True):
+    """Save the video with the annotated ball position (as a red dot). If `show_ground_truth` is True,
+    the ground truth is shown as a green dot.
+
+    Parameters
+    ----------
+    training_configuration : module
+        one of the modules in `train_configurations`
+    checkpoint_filename : str, optional
+        name of the checkpoint file in the checkpoint folder specified by the training configuration.
+        If not provided, the the best checkpoint is used.
+    split : str, optional
+        `'train'`, `'val'` or `'test'`. By default `'test'`
+    dataset_id : int or list of int, optional
+        Id of the dataset used in the selected split. By default 0
+    show_ground_truth : bool, optional
+        Show ground truth on the video. By default True
+
+    Raises
+    ------
+    FileNotFoundError
+        If the checkpoint folder specified in `training_configuration` is has no checkpoints of if it does not exist
+    FileNotFoundError
+        If the specified checkpoint does not have a results folder or if it is empty
+    ValueError
+        If `split` is something other than `'train'`, `'val'` or `'test'`
+    """
+    config = training_configuration.Config()   # get training configuration
+
+    if not os.path.exists(config._checkpoint_folder):
+        raise FileNotFoundError(f"Checkpoint folder not found: {config._checkpoint_folder}")
+
+    checkpoint_filenames = [name for name in os.listdir(config._checkpoint_folder) if name[-5:]=='.ckpt']
+
+    if len(checkpoint_filenames)==0:
+        raise FileNotFoundError(f"No checkpoint found in {config._checkpoint_folder}")
+
+    if checkpoint_filename is None:
+        for name in checkpoint_filenames:
+            if '_best' in name:
+                checkpoint_filename = name
+                break
+            checkpoint_filename = name
+
+    # create output folder inside the checkpoint folder
+    results_folder = '_'.join(checkpoint_filename.split('.')[0].split('_')[:2])
+    results_folder += '_results'
+    results_folder = os.path.join(config._checkpoint_folder, results_folder)
+
+    if not os.path.exists(results_folder):
+        raise FileNotFoundError("The results for the specified checkpoint do not exist. Run `create_output_csv` first.")
+
+    position_df_filename = os.path.join(results_folder, f'output_{split.lower()}.csv')
+    position_df = pd.read_csv(position_df_filename)
+
+    dataset_train, dataset_val, dataset_test = training_configuration.create_datasets()
+    if split.lower() == 'train':
+        dataset = dataset_train
+    elif split.lower() == 'val':
+        dataset = dataset_val
+    elif split.lower() == 'test':
+        dataset = dataset_test
+    else:
+        raise ValueError("The split must be either 'train', 'val', or 'test'")
+
+    heatmaps_folder = os.path.join(results_folder, f'heatmaps_{split.lower()}')
+    heatmaps_folder = heatmaps_folder if os.path.exists(heatmaps_folder) else None
+
+    dataset_info = dataset.get_info()
+
+    if dataset_id is None:
+        dataset_id = [i for i in range(len(dataset_info))]
+    elif type(dataset_id) is int:
+        dataset_id = [dataset_id]
+
+    print(f"Using data from {position_df_filename}\n")
+    for i in dataset_id:
+        src = os.path.join(dataset_info[i]['root'], 'video.mp4')
+        dst = os.path.join(results_folder, f'video_{split.lower()}_{i}.mp4')
+
+        print(f"Video {i+1} of {len(dataset_id)}")
+        print(f"src file: {src}")
+        print(f"dst file: {dst}")
+
+        create_video(filename_src=src,
+                     filename_dst=dst,
+                     position_df=position_df.loc[position_df['dataset_id']==i],
+                     show_ground_truth=show_ground_truth,
+                     heatmaps_folder=heatmaps_folder)
+        print()
