0da4a59ac38cfc93d09f1474bf3312ce46bc1adb
michele98 <calvanese.michele@gmail.com>
2023-06-15 12:36:17 +0200

    feat: add loss history and refactor visualization of loss

--- detection/visualization.py
+++ detection/visualization.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def show_loss_history(module):
+def get_loss_history(module):
+    """Get train loss and validation loss of the given module"""
     try:
         train_loss = np.loadtxt(os.path.join(module.Config()._checkpoint_folder, "loss_history.csv"))
         val_loss = np.loadtxt(os.path.join(module.Config()._checkpoint_folder, "loss_history_val.csv"))
@@ -21,18 +22,43 @@ def show_loss_history(module):
 
         train_loss = np.concatenate(train_loss)
         val_loss = np.concatenate(val_loss)
+    return train_loss, val_loss
 
-    fig, ax = plt.subplots()
+
+def show_loss_history(module, ax=None, epoch_range=None):
+    """Plot the loss history of the given module
+
+    Parameters
+    ----------
+    module : _type_
+        _description_
+    ax : matplotlib.axes.Axes, optional
+        _description_, by default None
+    epoch_range : tuple of int, shape (2,), optional
+        epoch range to plot, including both ends.
+        If not provided, the whole history is plotted
+
+    Returns
+    -------
+    matplotlib.axes.Axes
+    """
+    train_loss, val_loss = get_loss_history(module)
+
+    if epoch_range is not None:
+        train_loss = train_loss[epoch_range[0]-1:epoch_range[1]]
+        val_loss = val_loss[epoch_range[0]-1:epoch_range[1]]
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
     ax.set_yscale('log')
 
-    ax.plot(train_loss, label='train loss')
-    ax.plot(val_loss, label='val loss')
+    ax.plot(range(1, len(train_loss)+1), train_loss, '.-', label='train loss')
+    ax.plot(range(1, len(train_loss)+1), val_loss, '.-', label='val loss')
 
     ax.set_xlabel("Epoch")
     ax.set_ylabel("Loss")
 
     ax.legend()
 
-    fig.tight_layout()
-
-    plt.show()
+    return ax
